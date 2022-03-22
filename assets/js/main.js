bc6340a971c0968fa9925039d32abd728c693b62
dibss <ludovicad01@gmail.com>
2022-03-22 17:25:42 +0100

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,6 @@
-/*Consegna
-Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
-Il computer deve generare 16 numeri casuali nel range dei numeri della griglia: le bombe.
-I numeri nella lista delle bombe non possono essere duplicati.
-In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
-La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
-BONUS:
-1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
-2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
-3- l'utente indica un livello di difficoltà in base al quale viene generato un numero variabile di celle:
-con difficoltà 1 => tra 1 e 100
-con difficoltà 2 => tra 1 e 81
-con difficoltà 3 => tra 1 e 49
-Le bombe dovranno essere generate nello stesso range delle caselle di gioco.*/
-
 let grid = document.getElementById("grid");
 let paragraph = document.getElementById("welcome")
+game_over.classList.add("d-none");
 
 play.addEventListener("click",
   function play(){
@@ -27,6 +12,7 @@ play.addEventListener("click",
 
     grid.classList.remove("d-none");
     paragraph.classList.add("d-none");
+    game_over.classList.add("d-none");
   
     if(choice == "Easy"){
       cells = 100
@@ -62,9 +48,12 @@ play.addEventListener("click",
         box.addEventListener("click",
           function gameOver() {
             if (sixteenBombs.includes(parseInt(this.innerText))) {
-              this.classList.add('bomb');
-              box.innerHTML = `<i class="fa-solid fa-bomb"></i>`;
-              this.removeEventListener("click", gameOver);
+              for(let i = 0; i < sixteenBombs.length; i++){
+                game_over.classList.remove("d-none");
+                this.classList.add('bomb');
+                box.innerHTML = `<i class="fa-solid fa-bomb"></i>`;
+                this.removeEventListener("click", gameOver);
+              }
             } else {
               this.classList.add('clicked');
             }
